16dadb0e3e2d0e0638eb7f4302af28b8ee6fb7cb
amayones <mayonedrive1@gmail.com>
2026-06-03 15:47:13 +0700

    tes sns

--- index.js
+++ index.js
@@ -1,27 +1,78 @@
+
 const mysql = require('mysql2/promise');
+
+const {
+  SNSClient,
+  PublishCommand
+} = require('@aws-sdk/client-sns');
+
+const sns = new SNSClient({
+  region: 'us-east-1'
+});
 
 exports.handler = async (event) => {
 
-  const body = JSON.parse(event.body);
+  try {
 
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-  });
+    const body = JSON.parse(event.body);
 
-  await connection.execute(
-    'INSERT INTO users(name,email) VALUES (?,?)',
-    [body.name, body.email]
-  );
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+    });
 
-  await connection.end();
+    // INSERT USER
+    await connection.execute(
+      'INSERT INTO users(name,email) VALUES (?,?)',
+      [body.name, body.email]
+    );
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'User created'
-    })
-  };
+    await connection.end();
+
+    // SEND SNS EMAIL
+    await sns.send(
+      new PublishCommand({
+        TopicArn:
+          'arn:aws:sns:us-east-1:916470729977:UserNotification',
+
+        Subject:
+          'New User Created',
+
+        Message:
+`User baru berhasil dibuat
+
+Name: ${body.name}
+Email: ${body.email}`
+      })
+    );
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({
+        message: 'User created and SNS sent'
+      })
+    };
+
+  } catch (error) {
+
+    console.log(error);
+
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify({
+        message: 'Error',
+        error: error.message
+      })
+    };
+
+  }
+
 };
